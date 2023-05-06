ae6242fc87c543e2cf6413fec7256138e0d6241b
Pablo-Scattolin <122050191+Pablo-Scattolin@users.noreply.github.com>
2023-05-06 20:32:44 -0300

    Add files via upload

---
+++ api 2.0/src/main/java/com/portfolio/api/Security/Service/RolService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.api.Security.Service;
+
+import com.portfolio.api.Security.Entity.Rol;
+import com.portfolio.api.Security.Enums.RolNombre;
+import com.portfolio.api.Security.Repository.iRolRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
